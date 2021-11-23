a2e9f00dd6c987579e0a5e39bff28fdf11455d37
cuzknothz <cuzknothz@gmail.com>
2021-11-24 02:43:20 +0700

    fixz

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,62 @@
-import { Header } from "@/components/Header/Header";
-import { SideBar } from "@/components/SideBar";
+import React, { useEffect } from "react";
+import { Col, Container } from "react-bootstrap";
+import Video from "@/components/Video/Video";
+import CategoriesBar from "@/components/CategoriesBar/CategoriesBar";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getPopularVideos,
+  getVideosByCategory,
+} from "@/store/actions/videos.action";
 
-import { NextPage } from "next";
-import React from "react";
-import { HomeScreen } from "./homeScreen/HomeScreen";
+import InfiniteScroll from "react-infinite-scroll-component";
+import SkeletonVideo from "@/components/Skeletons/SkeletonVideo";
+import { useAppDispatch, useAppSelector } from "@/store";
 
-const HomePage: NextPage = () => {
+const HomeScreen = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getPopularVideos());
+  }, [dispatch]);
+
+  const { videos, activeCategory, loading } = useAppSelector(
+    (state) => state.homeVideos
+  );
+
+  const fetchData = () => {
+    if (activeCategory === "All") dispatch(getPopularVideos());
+    else {
+      dispatch(getVideosByCategory(activeCategory));
+    }
+  };
+
   return (
-    <>
-      {/* <Header />
-      <div className="mt-[50px]">
-        <SideBar />
-        <Videos />
-      </div> */}
-      <HomeScreen />
-    </>
+    <Container>
+      <CategoriesBar />
+
+      <InfiniteScroll
+        dataLength={videos.length}
+        next={fetchData}
+        hasMore={true}
+        loader={
+          <div className="spinner-border text-danger d-block mx-auto"></div>
+        }
+        className="row"
+      >
+        {!loading
+          ? //   @ts-ignore
+            videos.map((video, idx) => (
+              <div key={idx}>
+                {/* @ts-ignore */}
+                <Video video={video} key={video.id} />
+              </div>
+            ))
+          : [...Array(20)].map((i, idx) => (
+              <Col key={idx} lg={3} md={4}>
+                <SkeletonVideo />
+              </Col>
+            ))}
+      </InfiniteScroll>
+    </Container>
   );
 };
-
-export default HomePage;
+export default HomeScreen;
