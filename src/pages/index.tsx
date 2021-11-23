78f47812b46a164a50d89b5ee05fb30f638baa12
cuzknothz <cuzknothz@gmail.com>
2021-11-24 02:35:10 +0700

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/Header";
+import { Header } from "@/components/Header/Header";
 import { SideBar } from "@/components/SideBar";
 
 import { NextPage } from "next";
