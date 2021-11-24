f0f3acce9b4b13f83b3a5c3d8ca4155920d6c3df
cuzknothz <cuzknothz@gmail.com>
2021-11-24 20:32:48 +0700

    test api key

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: process.env.NEXT_PUBLIC_YT_API_KEY,
+    key: "AIzaSyBnTmhe4kjG5ixLB0v0YiNI7wHiuACOD4s",
   },
 });
 
